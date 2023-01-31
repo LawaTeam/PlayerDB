61820760f5ccaf9ce0ef047ddbbab75d2467f0eb
jingwenMC <2313055824@qq.com>
2023-01-31 11:36:07 +0800

    移动一个方法位置，防止造成误解

--- src/main/java/com/mclawa/playerdb/lang/PlayerLangManager.java
+++ src/main/java/com/mclawa/playerdb/lang/PlayerLangManager.java
@@ -1,6 +1,13 @@
 package com.mclawa.playerdb.lang;
 
+import net.md_5.bungee.api.ChatColor;
+import top.jingwenmc.spigotpie.common.SpigotPie;
+import top.jingwenmc.spigotpie.common.command.CommandSender;
+import top.jingwenmc.spigotpie.common.command.PieCommand;
 import top.jingwenmc.spigotpie.common.configuration.ConfigurationAdapter;
+import top.jingwenmc.spigotpie.common.instance.PieComponent;
+import top.jingwenmc.spigotpie.common.instance.Wire;
+import top.jingwenmc.spigotpie.spigot.configuration.SpigotConfigurationAdapter;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,9 +15,12 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+@PieComponent
 public class PlayerLangManager {
     protected static Map<String, ConfigurationAdapter> langMap = new ConcurrentHashMap<>();
     protected static Map<UUID,String> langCache = new ConcurrentHashMap<>();
+    @Wire
+    LangConfiguration langConfiguration;
 
     public static void init(File dataFolder,Class<? extends ConfigurationAdapter> configAdapterClass) throws Exception {
         File mainFolder = new File(dataFolder,"module_lang");
@@ -31,5 +41,17 @@ public class PlayerLangManager {
             }
             langMap.get(null).init(new File(mainFolder,"lang_"+s+".yml"));
         }
+    }
+
+    @PieCommand(value = "pdb lang reload",bungeeCord = false,spigot = true)
+    public void reload(CommandSender sender) throws Exception {
+        if(!sender.hasPermission("pdb.admin")) {
+            sender.sendMessage(ChatColor.RED+"[PERM] Access denied.");
+            return;
+        }
+        langConfiguration.reloadConfig();
+        PlayerLangManager.langMap = new ConcurrentHashMap<>();
+        PlayerLangManager.init(SpigotPie.getEnvironment().getWorkFolder(), SpigotConfigurationAdapter.class);
+        sender.sendMessage(ChatColor.GREEN+"[PDB] Reload complete.");
     }
 }
--- src/main/java/com/mclawa/playerdb/lang/SpigotCacheUpdateListener.java
+++ src/main/java/com/mclawa/playerdb/lang/SpigotCacheUpdateListener.java
@@ -31,8 +31,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SpigotCacheUpdateListener implements Listener {
     @Wire
     PlayerDB playerDB;
-    @Wire
-    LangConfiguration langConfiguration;
+
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
@@ -69,17 +68,5 @@ public class SpigotCacheUpdateListener implements Listener {
                 }
             }
         }.runTaskAsynchronously(playerDB);
-    }
-
-    @PieCommand(value = "pdb lang reload",bungeeCord = false,spigot = true)
-    public void reload(CommandSender sender) throws Exception {
-        if(!sender.hasPermission("pdb.admin")) {
-            sender.sendMessage(ChatColor.RED+"[PERM] Access denied.");
-            return;
-        }
-        langConfiguration.reloadConfig();
-        PlayerLangManager.langMap = new ConcurrentHashMap<>();
-        PlayerLangManager.init(SpigotPie.getEnvironment().getWorkFolder(), SpigotConfigurationAdapter.class);
-        sender.sendMessage(ChatColor.GREEN+"[PDB] Reload complete.");
     }
 }
